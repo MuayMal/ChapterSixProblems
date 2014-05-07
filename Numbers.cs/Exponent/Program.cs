f187731f28749b9a1a2a8f6247cd782ccdae86fc
MuayMal <mtoomer81@gmail.com>
2014-05-07 10:09:57 -0400

    lets go

---
+++ Numbers.cs/Exponent/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exponent
+{
+    class Program
+    {
+        public static int Square(int a)
+        {
+            
+
+            return (a);
+        }
+
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+        }
+    }
+}
--- Numbers.cs/Monogram.cs/Program.cs
+++ Numbers.cs/Monogram.cs/Program.cs
@@ -7,16 +7,30 @@ namespace Monogram.cs
 {
     class Program
     {
+        public static void ConsoleWriteLineDupe (string msg, params object[] parameters)
+        {
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                msg = msg.Replace("{" + i + "}", parameters[i].ToString());
+            }
 
-        public static void Monogram(string a, string b, string c, string d, string e, string f)
-    {
+            Console.WriteLine(msg);
+        }
 
-    }
+
+        public static void Monogram(char a, char b, char c, char d, char e, char f)
+        {
+
+           Console.WriteLine("** {0}.{1}.{2}. **",a,b,c);
+
+           Console.WriteLine("** {0}.{1}.{2}. **", d, e, f); 
+        }
 
         static void Main(string[] args)
         {
-            string f1 = "M", m1 = "Q", l1 = "T", f2 = "T", m2 = "L", l2 = "M";
+            char f1 = 'M', m1 = 'Q', l1 = 'T', f2 = 'T', m2 = 'L', l2 = 'M';
 
+            
             Monogram(f1,m1,l1,f2,m2,l2);
 
         }
